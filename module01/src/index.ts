a274945b64e769a3794d121c08b2c1d549fcd6f6
firoz.nur <firoznur5@gmail.com>
2025-05-16 09:46:52 +0600

    Add : Number

--- module01/src/index.ts
+++ module01/src/index.ts
@@ -2,3 +2,5 @@ let course: string = "Next Level Web Development";
 console.log(course);
 let isPublished: boolean = true;
 console.log("isPublished", isPublished);
+let credit: number = 4;
+console.log("credit", credit);
