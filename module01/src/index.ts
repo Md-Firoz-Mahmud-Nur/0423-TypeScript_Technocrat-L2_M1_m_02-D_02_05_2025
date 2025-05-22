1489c58c4e0a305514fc12ba6a7c81493c3d3f29
firoz.nur <firoznur5@gmail.com>
2025-05-22 09:49:58 +0600

    Add : Funtion

--- module01/src/index.ts
+++ module01/src/index.ts
@@ -4,3 +4,7 @@ let isPublished: boolean = true;
 console.log("isPublished", isPublished);
 let credit: number = 4;
 console.log("credit", credit);
+function add(a: number, b: number): number {
+  return a + b;
+}
+console.log(add(2, 5));
